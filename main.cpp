b9bdefa01495ad90b951d4e792e976279db8683f
Psycayer <crysis.zocker@gmx.de>
2017-12-10 16:14:29 +0100

    try to implement icon

--- main.cpp
+++ main.cpp
@@ -3,12 +3,20 @@
 
 int main(int argc, char **argv)
 {
- QApplication app (argc, argv);
+    QApplication app (argc, argv);
 
- QPushButton button;
- button.setText("Whoop whoop 'n Knöpges!");
- button.setToolTip("Des is n richtig harter Knöpges. Der kann zwar nich viel, aber das kann er gut.");
- button.show();
+    QPushButton button;
+    button.setText("Whoop whoop 'n Knöpges!");
+    button.setToolTip("Des is n richtig harter Knöpges. Der kann zwar nich viel, aber das kann er gut.");
+    button.show();
 
- return app.exec();
+    QFont font("Arial",20,-1,true);
+    button.setFont(font);
+
+    QIcon icon ("/icon/83825-200.png");
+    button.setIcon(icon);
+
+    button.setIcon(QIcon::fromTheme("face-smile"));
+
+    return app.exec();
 }
