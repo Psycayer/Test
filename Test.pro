c3030612a458ddeeb02544384cc2ef771739267f
Kuschranada <kuschranini@web.de>
2017-12-31 15:52:00 +0100

    Added pointer example

--- Test.pro
+++ Test.pro
@@ -6,7 +6,5 @@ QT = core gui
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 SOURCES +=  main.cpp \
-    window.cpp
 
 HEADERS += \
-    window.h
--- main.cpp
+++ main.cpp
@@ -1,27 +1,33 @@
-#include <QApplication>
-#include <QPushButton>
-#include <QFont>
-#include <QString>
-#include <window.h>
 
-int main(int argc, char **argv)
+#include <iostream>
+
+int foo = 200;
+
+void
+huggabugga(int* zahl)
 {
- QApplication app (argc, argv);
+  int Wert = 20;
+  std::cout << "Was steht in zahl = " << zahl << std::endl;
+  std::cout << "Was steht in *zahl = " << *zahl << std::endl;
+  std::cout << "Was steht in &zahl = " << &zahl << std::endl;
+  // int ergb = zahl + Wert;
+  foo = 100;
+}
 
- //QFont(const QString & family, int pointSize = –1, int weight = -1, bool italic = false);
+main(int argc, char** argv)
+{
+  int Wert = 5;
+  int* pWertAdresse = &Wert;
+  // int Adresse = &Wert;
+  // int* pWert = Wert;
 
-/*
- QFont font ("Courier");
+  // std::cout << "Die inhalt aus Adresse ist = " << pWert << std::endl; // 5
 
- QPushButton button;
- button.setText("My text");
- button.setToolTip("A tooltip");
- button.setFont(font);
- button.show();
-*/
+  std::cout << "Der Variablen Inhalt ist = " << Wert << std::endl; // 5
+  // std::cout << "Der Speicheradresse = " << Adresse << std::endl;   // 546846
+  std::cout << "Die Speicheradresse ist = " << pWertAdresse
+            << std::endl;                              // 123486382
+  std::cout << "??? = " << *pWertAdresse << std::endl; // 123486382
 
- Window window;
- window.show();
-
- return app.exec();
+  huggabugga(&Wert);
 }
