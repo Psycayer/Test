669e6b2b192da0bcbb210c5f9a24665effe04061
Psycayer <crysis.zocker@gmx.de>
2018-01-08 19:34:19 +0100

    Projektreset

--- main.cpp
+++ main.cpp
@@ -1,35 +1,11 @@
+#include "mainwindow.h"
 #include <QApplication>
-#include "window.h"
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    QApplication app (argc, argv);
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-    Window window;
-    window.show();
-
-    return app.exec();
+    return a.exec();
 }
-
-/*
- QWidget window;
- window.setFixedSize(1000, 500);
- QPushButton *button = new QPushButton("Hello World", &window);
- button->setGeometry(100, 100, 100, 100);
- window.show();                                    Fenster
-
- QPushButton button1 ("test");
- QPushButton button2 ("other", &button1);
- button1.show();                                   Buttonception
-
- QPushButton button;
- button.setText("Whoop whoop 'n Knöpges!");
- button.setToolTip("Des is n richtig harter Knöpges. Der kann zwar nich viel, aber das kann er gut.");
- button.show();
- QFont font("Arial",20,-1,true);
- button.setFont(font);                             Knöpges
-
-  QIcon icon ("/icon/83825-200.png");
-  button.setIcon(icon);
-  button.setIcon(QIcon::fromTheme("face-smile"));  funzt nicht!
-*/
