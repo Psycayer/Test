bada5d05e8722a7c6edfd7c9b31867385becd0b8
Kuschranada <kuschranini@web.de>
2018-01-08 20:43:11 +0100

    work in progres

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@ int foo = 200;
 void
 huggabugga(int* zahl)
 {
-  int Wert = 20;
-  std::cout << "Was steht in zahl = " << zahl << std::endl;
-  std::cout << "Was steht in *zahl = " << *zahl << std::endl;
+  std::cout
+    << "zahl gibt die Speicheradresse zurück auf die der Pointer zeigt = "
+    << zahl << std::endl;
+  std::cout << "*zahl Gibt den Wert aus auf den der Pointer zeigt =" << *zahl
+            << std::endl;
   std::cout << "Was steht in &zahl = " << &zahl << std::endl;
   // int ergb = zahl + Wert;
   foo = 100;
@@ -17,17 +19,6 @@ huggabugga(int* zahl)
 main(int argc, char** argv)
 {
   int Wert = 5;
-  int* pWertAdresse = &Wert;
-  // int Adresse = &Wert;
-  // int* pWert = Wert;
-
-  // std::cout << "Die inhalt aus Adresse ist = " << pWert << std::endl; // 5
-
-  std::cout << "Der Variablen Inhalt ist = " << Wert << std::endl; // 5
-  // std::cout << "Der Speicheradresse = " << Adresse << std::endl;   // 546846
-  std::cout << "Die Speicheradresse ist = " << pWertAdresse
-            << std::endl;                              // 123486382
-  std::cout << "??? = " << *pWertAdresse << std::endl; // 123486382
-
+  std::cout << "Speicheradresse von Wert = " << &Wert << std::endl;
   huggabugga(&Wert);
 }
