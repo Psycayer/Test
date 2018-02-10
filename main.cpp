a3c17002a342b6694e041c84ef32089d4c10302b
Kuschranada <kuschranini@web.de>
2018-02-10 17:06:52 +0100

    letzte Änderungen

--- main.cpp
+++ main.cpp
@@ -3,22 +3,21 @@
 
 int foo = 200;
 
-void
-huggabugga(int* zahl)
-{
+void huggabugga(int *zahl) {
   std::cout
-    << "zahl gibt die Speicheradresse zurück auf die der Pointer zeigt = "
-    << zahl << std::endl;
-  std::cout << "*zahl Gibt den Wert aus auf den der Pointer zeigt =" << *zahl
-            << std::endl;
-  std::cout << "Was steht in &zahl = " << &zahl << std::endl;
-  // int ergb = zahl + Wert;
+      << "zahl gibt die Speicheradresse zurück auf die der Pointer zeigt = " << zahl << std::endl;
+  std::cout << "*zahl Gibt den Wert aus auf den der Pointer zeigt =" << *zahl << std::endl;
+  std::cout << "Was steht in &zahl = " << &zahl << std::endl; // int ergb = zahl + Wert;
   foo = 100;
 }
 
-main(int argc, char** argv)
-{
+main(int argc, char **argv) {
+
   int Wert = 5;
   std::cout << "Speicheradresse von Wert = " << &Wert << std::endl;
   huggabugga(&Wert);
+
+  return 0;
 }
+
+// /usr/bin/clang-format
